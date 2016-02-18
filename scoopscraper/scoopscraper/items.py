e9c3fea8ac8adaa7ab0d77947953b66b513c4da5
Kyle Hebert <kyle.hebert@gmail.com>
2016-02-17 20:59:24 -0600

    added item and spider

--- scoopscraper/scoopscraper/items.py
+++ scoopscraper/scoopscraper/items.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy.item import Item, Field
 
 
-class ScoopscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ScoopscraperItem(Item):
+    # Primary Fields
+    flavor_name = Field()
+    flavor_description = Field()
+    date_seen = Field()
+
+    # Debug Fields
+    url = Field()
--- scoopscraper/scoopscraper/spiders/izzy_spider.py
+++ scoopscraper/scoopscraper/spiders/izzy_spider.py
@@ -1,13 +1,18 @@
-# -*- coding: utf-8 -*-
 import scrapy
+from scoopscraper.items import ScoopscraperItem
 
 
-class IzzySpiderSpider(scrapy.Spider):
+class IzzySpider(scrapy.Spider):
     name = "izzy_spider"
     allowed_domains = ["izzysicecream.com"]
     start_urls = (
-        'http://www.izzysicecream.com/',
+        'http://flavorup.izzysicecream.com/flavor-grid/web/minneapolis',
     )
-
+  
     def parse(self, response):
-        pass
+        item = ScoopscraperItem()
+        for flavor in response.xpath('//*[@id="grid"]/ul/li'): 
+            item['flavor_name'] = flavor.xpath('.//p[2]/text()').extract()
+            item['flavor_description'] = flavor.xpath(
+                './/div/p[2]/text()').extract()
+            yield item
