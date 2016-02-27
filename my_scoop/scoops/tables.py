70b6b5703237df17c7b042be7d750a1561955877
Kyle Hebert <kyle.hebert@gmail.com>
2016-02-27 13:40:49 -0600

    modified table layout

--- my_scoop/scoops/tables.py
+++ my_scoop/scoops/tables.py
@@ -7,9 +7,9 @@ from .models import IzzyFlavor
 class IzzyFlavorTable(tables.Table):
     flavor_name = tables.LinkColumn('scoops:flavor_detail_view', args=[A('pk')],
                                     verbose_name="Flavor",)
-    flavor_description = tables.Column(verbose_name="Description")
+    # flavor_description = tables.Column(verbose_name="Description")
     location = tables.Column(verbose_name="Location")
     
     class Meta:
         model = IzzyFlavor
-        exclude = ('id', 'date_seen', 'url')
+        exclude = ('id', 'date_seen', 'url', 'flavor_description')
