ea36b26a409853756e7063d1a7ef5b13b51ef9ca
Kyle Hebert <kyle.hebert@gmail.com>
2016-02-27 13:59:26 -0600

    commenting for clarity

--- my_scoop/scoops/views.py
+++ my_scoop/scoops/views.py
@@ -6,11 +6,15 @@ from .tables import IzzyFlavorTable
 
 
 def index(request):
+    """Uses django_tables2 to render a table of flavors and locations"""
+    # get a queryset of all flavors
     table = IzzyFlavorTable(IzzyFlavor.objects.all())
-    RequestConfig(request).configure(table)
+    # set to 32 flavors per page (1 page per location)
+    RequestConfig(request, paginate={'per_page': 32}).configure(table)
     return render(request, 'scoops/index.html', {'table': table})
 
 
 def flavor_detail(request, pk):
+    # get the flavor by primary key
     flavor = IzzyFlavor.objects.get(pk=pk)
     return render(request, 'scoops/flavor_detail.html', {'flavor': flavor})
